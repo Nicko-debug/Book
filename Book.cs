fad3cd412be41c2d23f4e5f379b5b007a88e23a6
Nicko-debug <82638022+Nicko-debug@users.noreply.github.com>
2021-04-16 14:51:55 +0800

    Added Program.cs

---
+++ Book.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Book
+{
+    static public void Main ()
+    {
+        Console.WriteLine ("Welcome to my Book Management System");
+     
+      string Book1 = "Harry Potter and half blood prince";
+      
+       
+       Console.Write("Enter Book Title: ");
+string bookInfo = Console.ReadLine();
+       
+  if (bookInfo == Book1){
+           Console.WriteLine(bookInfo);
+Console.WriteLine("*Author J.K Rowling");
+Console.WriteLine("*IBSN: ABC123");
+Console.WriteLine("*Edition: 2000");
+Console.WriteLine("*Publisher: Bloomsberry");
+  
+   
+        
+       
+
+    }
+
+    }
+
+}
